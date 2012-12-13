00d06b9a855760c8dfddce429cfc5afe6907ae12
Peter Wortmann <scpmw@leeds.ac.uk>
2012-12-13 13:30:41 +0000

    Prevent aliases from causing name collisions
    Really should have seen this one coming. Dollar suffixes should be safe
    to the best of my knowledge.

--- compiler/llvmGen/LlvmCodeGen/Base.hs
+++ compiler/llvmGen/LlvmCodeGen/Base.hs
@@ -448,8 +448,8 @@ getGlobalPtr llvmLbl = do
     Nothing -> do
       delayType llvmLbl
       return $ mkGlbVar
-        (llvmLbl `appendFS` fsLit "_alias")
-        (LMAlias (llvmLbl `appendFS` fsLit "_type", undefined))
+        (llvmLbl `appendFS` fsLit "$alias")
+        (LMAlias (llvmLbl `appendFS` fsLit "$type", undefined))
         Alias
 
 -- | Generate aliases for references that were created while compiling.
@@ -465,8 +465,8 @@ generateAliases = do
           Nothing -> let ty = LMArray 0 llvmWord
                          var = mkVar ty External
                      in ([LMGlobal var Nothing], ty, var)
-        aliasLbl = lbl `appendFS` fsLit "_alias"
-        tyLbl = lbl `appendFS` fsLit "_type"
+        aliasLbl = lbl `appendFS` fsLit "$alias"
+        tyLbl = lbl `appendFS` fsLit "$type"
         aliasVar = LMGlobalVar aliasLbl (LMPointer ty) Internal Nothing Nothing Alias
     return ((LMGlobal aliasVar $ Just $ LMStaticPointer var) : defs,
             LMAlias (tyLbl, ty)
@@ -501,10 +501,10 @@ generateAliases = do
 -- refer to "fun" itself, but instead refer to a *value* alias of it instead,
 -- which we can later set to the proper value without any further hassle:
 --
---  @ptr = constant %typ* @fun_alias;
+--  @ptr = constant %typ* @fun$alias;
 --  define i32 @fun() { ret i32 0; }
 --  %typ = type i32 ();
---  @fun_alias = alias %typ* @fun;
+--  @fun$alias = alias %typ* @fun;
 
 -- ----------------------------------------------------------------------------
 -- * Misc
